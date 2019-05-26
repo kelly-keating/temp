942c0aad30daadc41aa61a339e2ca3a76ba729fa
Kelly <keating.kel@gmail.com>
2019-05-27 11:18:10 +1200

    Create index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+const Octokit = require('@octokit/rest')
+
+const clientWithAuth = new Octokit({
+  auth: 'token XXX'
+})
+
+let octokit = clientWithAuth
+
+let usernames = [
+  'ben-sherwood',
+  'colleenteau',
+  'Danbez13',
+  'daniel-bridewell',
+  'Danielrhodesy',
+  'hamish-w-smith',
+  'hanh008',
+  'john-w-aupouri',
+  'karla-jane-hogan',
+  'KandaceW',
+  'mikeywipo',
+  'OliverPetri',
+  'haluskua',
+  'pano-skylakis',
+  'daniel-vanammers',
+  'quentin-papera',
+  'reubenosborne',
+  'tim-xa',
+  'tess-meyer',
+  'tom-doesburg',
+  'tessajane',
+  'serenade-akaata'
+]
+
+let org = 'kotare-2019'
+let role = 'member' // 'admin' or 'member'
+
+Promise.all(
+  usernames
+    .sort()
+    .map(username => octokit.orgs.addOrUpdateMembership({ org, username, role }))
+)
+  .then(results => {
+    results
+      .forEach(result => {
+        // eslint-disable-next-line no-console
+        console.log({ status: result.status }, result.data)
+      })
+  })
+  .catch(err => {
+    // eslint-disable-next-line no-console
+    console.error(err)
+  })
